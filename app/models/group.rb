184a3bd9df802993377c5059f5fd3ebf4e4f8fce
koo1129 <koo.1192icloud.com>
2020-03-12 14:28:47 +0900

    comit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,20 @@
 class Group < ApplicationRecord
     has_many :group_users
     has_many :users, through: :group_users
-    validates :name, presence: true, uniqueness: true
+    has_many :messages
+    
+    validates :name, presence: true
+
+    def show_last_message
+        if (last_message = messages.last).present?
+            if last_message.content?
+                last_message.content
+            else
+                '画像が表示されていません'
+            end
+        else
+            'まだメッセージはありません'
+        end
+    end
+
 end
